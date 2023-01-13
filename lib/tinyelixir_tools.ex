512d2220fc269e5ddb5c16098b1046c5c3ddaa19
Tim Mecklem <timothy@mecklem.com>
2023-01-12 22:31:29 -0500

    mix new

---
+++ lib/tinyelixir_tools.ex
@@ -0,0 +1,18 @@
+defmodule TinyelixirTools do
+  @moduledoc """
+  Documentation for `TinyelixirTools`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TinyelixirTools.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/tinyelixir_tools_test.exs
@@ -0,0 +1,8 @@
+defmodule TinyelixirToolsTest do
+  use ExUnit.Case
+  doctest TinyelixirTools
+
+  test "greets the world" do
+    assert TinyelixirTools.hello() == :world
+  end
+end
